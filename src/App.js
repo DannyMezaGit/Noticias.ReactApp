5561b056a9105edfef564464fe2cff5c6f474392
Daniel Meza <danielmeza.4c@gmail.com>
2020-01-08 11:01:47 -0700

    Separar peticion dentro de un metodo indepente

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ class App extends Component {
       noticias: []
    }
 
+   componentDidMount() {
+    this.consultarNoticias();
+  }
 
-  async componentDidMount() {
-    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=aeed65fd3ce9447cb791939989029810`
+  consultarNoticias = async () => {
+   const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=aeed65fd3ce9447cb791939989029810`
 
-    const res = await fetch(url);
-    const noticias = await res.json();
+   const res = await fetch(url);
+   const noticias = await res.json();
 
-    console.log(noticias.articles);
+   console.log(noticias.articles);
 
-    this.setState({
-      noticias: noticias.articles
-    })
-
+   this.setState({
+     noticias: noticias.articles
+   })
   }
 
   render() { 
-    return ( <h1>Noticias</h1> );
+    return ( <h1>Noticias</h1> )
   }
 }
- 
+
 export default App;
