0ef50f4d4e5ce7b7182f526530c5e97854a260b6
Daniel Meza <danielmeza.4c@gmail.com>
2020-01-08 12:20:48 -0700

    Buscador de Categorias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Header from './components/Header';
 import ListaNoticias from './components/ListaNoticias';
+import Formulario from './components/Formulario';
 
 
 class App extends Component {
@@ -32,6 +33,7 @@ class App extends Component {
           titulo = 'Noticias React API'
         />
         <div className="container white contenedor-noticias">
+            <Formulario />
             <ListaNoticias 
               noticias = { this.state.noticias }
             />
