1bbe9f1b64e4b5e2fa5c4800324a1b97abcac6bf
Daniel Meza <danielmeza.4c@gmail.com>
2020-01-08 10:55:56 -0700

    Peticion de noticias a API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  state = {  }
+  state = { 
+      noticias: []
+   }
 
+
+  async componentDidMount() {
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=aeed65fd3ce9447cb791939989029810`
+
+    const res = await fetch(url);
+    const noticias = await res.json();
+
+    console.log(noticias.articles);
+
+    this.setState({
+      noticias: noticias.articles
+    })
+
+  }
 
   render() { 
     return ( <h1>Noticias</h1> );
